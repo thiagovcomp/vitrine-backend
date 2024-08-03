fb167e41629a50d01bfb50330a95d24b2b8f111c
thiago <thiagovcomp@gmail.com>
2024-08-03 15:08:18 -0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -11,10 +11,10 @@ app.use(cors());
 
 app.use(router);
 
-app.use(
-  '/files',
-  express.static(path.resolve(__dirname, '..', 'tmp'))
-)
+// app.use(
+//   '/files',
+//   express.static(path.resolve(__dirname, '..', 'tmp'))
+// )
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if(err instanceof Error){
