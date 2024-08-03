179b9c74a3bae4a24944f044d759d066b517254a
thiago <thiagovcomp@gmail.com>
2024-08-03 15:41:28 -0300

    deploy

--- src/routes.ts
+++ src/routes.ts
@@ -34,7 +34,7 @@ const router = Router();
 //-- ROTAS USER --
 // router.post('/users', new CreateUserController().handle)
 
-router.post('/session', new AuthUserController().handle)
+// router.post('/session', new AuthUserController().handle)
 
 // router.get('/me', isAuthenticated,  new DetailUserController().handle )
 // router.get('/users/lessAnyLogged', isAuthenticated,  new FindUserLessAnyLoggedController().handle )
