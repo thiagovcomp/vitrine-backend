41370ce8abc4f1b35dd5b62bf63eaeddc5daad37
thiago <thiagovcomp@gmail.com>
2024-08-03 15:37:08 -0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,8 @@ import { router } from './routes';
 try{
     console.log('aq');
     const app = express();
-    // app.use(express.json());
-    // app.use(cors());
+    app.use(express.json());
+    app.use(cors());
     
     // app.use(router);
     
