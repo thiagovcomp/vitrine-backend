ea658312e07e487c067c2909137cf440c59d7d68
thiago <thiagovcomp@gmail.com>
2024-08-05 11:43:57 -0300

    de

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -11,46 +11,46 @@ interface UserRequest{
 class CreateUserService{
   async execute({ name, email, password }: UserRequest){
 
-    // verificar se ele enviou um email
-    if(!email){
-      throw new Error("Email obrigatório")
-    }
+    // // verificar se ele enviou um email
+    // if(!email){
+    //   throw new Error("Email obrigatório")
+    // }
  
-    //Verificar se esse email já está cadastrado na plataforma
-    const userAlreadyExists = await prismaClient.user.findFirst({
-      where:{
-        email: email
-      }
-    })
+    // //Verificar se esse email já está cadastrado na plataforma
+    // const userAlreadyExists = await prismaClient.user.findFirst({
+    //   where:{
+    //     email: email
+    //   }
+    // })
 
-    if(userAlreadyExists){
-      throw new Error("Email já cadastrado.")
-    }
+    // if(userAlreadyExists){
+    //   throw new Error("Email já cadastrado.")
+    // }
 
-    const createCompanyService = new CreateCompanyService();
+    // const createCompanyService = new CreateCompanyService();
 
-    const company = await createCompanyService.execute({
-      name
-    });
+    // const company = await createCompanyService.execute({
+    //   name
+    // });
 
-    const passwordHash = await hash(password, 8)
+    // const passwordHash = await hash(password, 8)
 
-    const user = await prismaClient.user.create({
-      data:{
-        company_id: company.id,
-        name: name,
-        email: email,
-        password: passwordHash,
-      },
-      select:{
-        uuid: true,
-        name: true,       
-        email: true,
-      }
-    })
+    // const user = await prismaClient.user.create({
+    //   data:{
+    //     company_id: company.id,
+    //     name: name,
+    //     email: email,
+    //     password: passwordHash,
+    //   },
+    //   select:{
+    //     uuid: true,
+    //     name: true,       
+    //     email: true,
+    //   }
+    // })
 
 
-    return user;
+    // return user;
   }
 }
 
